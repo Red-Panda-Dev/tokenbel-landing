ce8053bfcfb2a423e56ae135084953d150916d3e
Andrei <drop.all.requests@gmail.com>
2026-01-07 21:57:07 +0300

    upd

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,7 +15,6 @@ export default {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
           DEFAULT: "hsl(var(--primary))",
